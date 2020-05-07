ef32f1aba448180871c80bbb04510a41aa2d7209
zzb <zzb@DESKTOP-6OURS1H>
2020-05-07 17:23:49 +0800

    终于弄好了

--- III_刷题/刷题/Main.java
+++ III_刷题/刷题/Main.java
@@ -1,7 +1,7 @@
 /**
  * Git测试完美成功
  */
-import java.util.Scanner;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author 张致邦
@@ -9,49 +9,11 @@ import java.util.Scanner;
  * 2020年3月16日-下午8:43:34
  */
 public class Main {
-	private static char n;
+	// 更新
 	
-	public static void main(String[] args) {
-		System.out.println(n);
-//		fun();
-	}
-	
-	private static void fun() {
-		Scanner in = new Scanner(System.in);
-		int m = in.nextInt(), ans = 0;
-		int[] arr = new int[m];
-		for (int i = 0; i < m; i++) {
-			arr[i] = in.nextInt();
-		}
-		
-		while (!test(arr)) {
-			
-			int[] temp = new int[m];
-			for (int i = 0; i < m; i++) {
-				int t = (i+1)%m;
-				temp[i] = arr[t]/2+arr[i]/2;
-				ans += temp[i]%2;
-				temp[i] += temp[i]%2;
-			}
-			
-			arr = temp;	
-		}
-		
-		in.close();
-		System.out.println(ans);
-	}
-
-	private static boolean test(int[] arr) {
-		int n = arr[0];
-		
-		for (int temp: arr) {
-			if (temp != n)
-				return false;
-		}
-		
-		return true;
-	}
-
-
-	
+	@Test
+	public void Test() {
+		double a = 9;
+    }
+    
 }
